8d77aa3297337cff3ff703c814c2ce3747d9f2ac
Vigeash Gobal <vigeash11@gmail.com>
2025-07-22 14:53:02 -0400

    fix(ui): remove all yellow backgrounds from home page, set everything to white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
 
   return (
     <div className="min-h-screen flex flex-col bg-white">
-      <main className="flex-1 flex flex-col items-center justify-center w-full">
+      <main className="flex-1 flex flex-col items-center justify-center w-full bg-white">
         {userId ? (
           <div className="flex flex-col justify-center items-center w-full h-full max-w-3xl mx-auto px-4" style={{ minHeight: '70vh' }}>
             <div className="flex-1 w-full flex flex-col justify-end">
