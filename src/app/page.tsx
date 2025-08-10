6b97d1af86f27f769f31d528d98c32771a2f04ed
Vigeash Gobal <vigeash11@gmail.com>
2025-08-10 19:31:18 -0400

    fix: resolve chat UI scrolling issue by adding proper height constraints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default async function Home() {
   const { userId } = await auth();
 
   return (
-    <div className="flex flex-col bg-white w-full h-full flex-1 min-h-0">
-      <main className="flex-1 min-h-0 w-full bg-white flex flex-col p-0 m-0">
+    <div className="flex flex-col bg-white w-full h-screen min-h-0">
+      <main className="flex-1 min-h-0 w-full bg-white flex flex-col p-0 m-0 overflow-hidden">
         {userId ? (
           <ChatWithSidebar />
         ) : (
